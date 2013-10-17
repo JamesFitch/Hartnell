d80a69323bd03bb60f3345a61a9dcdc16dd490fe
Lisa Ann Jansen <lisajansen@serverlogic.com>
2013-10-16 19:30:25 -0700

    Unexpected Omegas issues Chrome: Resolved

--- sites/all/themes/omega_hartnell/layouts/internal/internal.tpl.php
+++ sites/all/themes/omega_hartnell/layouts/internal/internal.tpl.php
@@ -21,11 +21,15 @@
 
 <div class="omega-grid clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-    <div class="grid-13 push-3 alpha">
-        
-        <div class="clearfix">
-            <div class="inside"><?php print $content['content']; ?></div>
-        
+    <div class="panel-panel grid-13 push-3"> 
+
+        <div class="grid-13 alpha omega row1">
+            <div class="inside">
+                <?php print $content['content']; ?>
+            </div> <!-- /.inside -->
+        </div> <!-- /.grid-13 .row1 -->
+
+        <div class="grid-13 alpha omega row2">
             <div class="grid-4 alpha omega">
                 <div class="inside"><?php print $content['lower_left']; ?></div>
             </div>
@@ -35,24 +39,32 @@
             <div class="grid-4  omega">
                 <div class="inside"><?php print $content['lower_right']; ?></div>
             </div>
-        </div>
+        </div><!-- /.grid-13 .row2 -->
+        
+        <div class="grid-13 alpha omega row3">
+           <div class="inside">
+                <?php print $content['content_2']; ?>
+           </div> <!-- /.inside -->
+        </div> <!-- /.grid-13 .row3 -->
 
-        <div class="inside"><?php print $content['content_2']; ?></div>
+        <div class="grid-13 alpha omega row4">
+            <div class="grid-4 alpha omega">
+                <div class="inside"><?php print $content['lower_left_2']; ?></div>
+            </div>
+            <div class="grid-5 omega">
+                <div class="inside"><?php print $content['lower_middle_2']; ?></div>
+            </div>
+            <div class="grid-4 omega">
+                <div class="inside"><?php print $content['lower_right_2']; ?></div>
+            </div>
+        </div> <!-- /.grid-13 .row4 -->
 
-        <div class="grid-4 alpha omega">
-            <div class="inside"><?php print $content['lower_left_2']; ?></div>
-        </div>
-        <div class="grid-5  omega">
-            <div class="inside"><?php print $content['lower_middle_2']; ?></div>
-        </div>
-        <div class="grid-4  omega">
-            <div class="inside"><?php print $content['lower_right_2']; ?></div>
-        </div>
+    </div> <!--/.panel-panel .grid-13 .push-3 -->
 
-    </div>
+    <div class="grid-3 pull-13 sidebar">
+        <div class="inside">
+            <?php print $content['left']; ?>
+        </div><!-- /.inside -->
+    </div><!--/.grid-3 .pull-13 .sidebar -->
 
-    <div class="grid-3 pull-13 sidebar omega">
-        <div class="inside"><?php print $content['left']; ?></div>
-    </div>
-
-</div>
+</div><!-- /.omega-grid .clearfix-->
